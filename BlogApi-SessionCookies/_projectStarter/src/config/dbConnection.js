368fc8687e990da216f72a6a044cb01a1b47b062
payas <sehzade172041@gmail.com>
2024-12-08 09:15:34 +0300

    update

--- BlogApi-SessionCookies/_projectStarter/src/config/dbConnection.js
+++ BlogApi-SessionCookies/_projectStarter/src/config/dbConnection.js
@@ -1,7 +1,5 @@
 "use strict";
-/* -------------------------------------------------------
-    EXPRESSJS - BLOG Project with Mongoose
-------------------------------------------------------- */
+
 
 const mongoose = require("mongoose");
 const { CustomError } = require("../errors/customError");
