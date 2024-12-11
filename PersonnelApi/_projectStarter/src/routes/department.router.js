46b3a78ed9b8ea41d355ba51c73911d8cf7bb60e
payas <sehzade172041@gmail.com>
2024-12-12 00:05:10 +0300

    up

--- PersonnelApi/_projectStarter/src/routes/department.router.js
+++ PersonnelApi/_projectStarter/src/routes/department.router.js
@@ -1,11 +1,23 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
 
-
+const department = require("../controllers/department.controller");
 
 /* ------------------------------------------------------- */
-module.exports = router
+router.route("/").get(department.list).post(department.create);
+
+router
+  .route("/:id")
+  .get(department.read)
+  .put(department.update)
+  .patch(department.update)
+  .delete(department.delete);
+
+//! /department/:id/personnel
+
+router.get("/:id/personnel",department.personnels)
+module.exports = router;
--- PersonnelApi/_projectStarter/src/routes/personnel.router.js
+++ PersonnelApi/_projectStarter/src/routes/personnel.router.js
@@ -1,11 +1,28 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
 
+const personnel = require("../controllers/personnel.controller");
+const idValidation = require("../middlewares/idValidation");
 
+// URL: /personnels
+//http://localhost:8000/personnels/login
+
+//! Login Logout
+router.post("/login", personnel.login);
+router.all("/logout", personnel.logout);
+
+router.route("/").get(personnel.list).post(personnel.create);
+
+router
+  .route("/:id")
+  .get(idValidation, personnel.read)
+  .put(personnel.update)
+  .patch(personnel.update)
+  .delete(personnel.delete);
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
