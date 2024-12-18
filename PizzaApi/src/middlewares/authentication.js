1f2962eb447623bc3067af94b537f840c3f4bc8e
payas <sehzade172041@gmail.com>
2024-12-18 19:36:47 +0300

    update

--- PizzaApi/src/middlewares/authentication.js
+++ PizzaApi/src/middlewares/authentication.js
@@ -6,5 +6,14 @@
 const Token = require("../models/token");
 
 module.exports = async (req, res, next) => {
+  const auth = req.headers?.authorization || null;
+  const tokenKey = auth ? auth.split(" ") : null;
+
+  if (token && tokenKey[0] == "Token") {
+    const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+      "userId"
+    );
+    if (tokenData) req.user = tokenData.userId;
+  }
   next();
 };
