f94c3e12ce9137721fb0b86f174422aec196e253
payas <sehzade172041@gmail.com>
2024-12-26 21:41:34 +0300

    update

--- STOCKAPİ/src/models/user.js
+++ STOCKAPİ/src/models/user.js
@@ -1,6 +1,54 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 /* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    index: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    set: (password) => passwordEncrypt(password),
+  },
+  email: {
+    type: String,
+    required: true,
+    uniqeu: true,
+    trim: true,
+    validate: [
+      (email) => email.includes("@") && email.includes("."),
+      "Email is not valid",
+    ],
+  },
+  firstName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  lastName: {
+    type: true,
+    trim: true,
+    required: true,
+  },
+  isStaff: {
+    type: Boolean,
+    default: false,
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+},
+{ collection: "user", timestamps: true});
+
+module.exports = mongoose.model("User", UserSchema);
