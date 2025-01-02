b9078fcfee8e6b0ec4cabad13846e5721b91a201
payas <sehzade172041@gmail.com>
2025-01-02 08:56:39 +0300

    update

--- STOCKAPİ/src/models/user.js
+++ STOCKAPİ/src/models/user.js
@@ -66,35 +66,28 @@ const UserSchema = new mongoose.Schema(
 UserSchema.pre(["save", "updateOne"], function (next) {
   // console.log('this is from pre middleware');
   // console.log(this);
-
   // updateOne: _update, save: this
   const data = this?._update ?? this;
-
   // Email Validation:
   const isEmailValidated = data.email
     ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email)
     : true;
-
   if (!isEmailValidated) {
     // throw new Error('Email is not validated');
     next(new Error("Email is not validated"));
   }
-
   const isPasswordValidated = data.password
     ? /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.]).{8,}$/.test(
         data.password
       )
     : true;
-
   if (!isPasswordValidated)
     next(
       new Error(
         "Password must be at least 8 characters long and contain at least one special character and  at least one uppercase character."
       )
     );
-
   if (data.password) data.password = passwordEncrypt(data.password);
-
   next();
 });
 
