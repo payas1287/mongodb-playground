946d814164bef1ea322be9c5c06d233a6c079364
payas <sehzade172041@gmail.com>
2025-01-09 09:29:23 +0300

    update

---
+++ 31-Todo_Template/app/controllers/todo.controller.view.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const Todo = require("../models/todo.model");
+
+const PRIORITIES = {
+  "-1": "Low",
+  0: "Normal",
+  1: "High",
+};
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await Todo.findAndCountAll();
+    res.render("index", {
+      todod: data.rows,
+      count: data.count,
+      priorities: PRIORITIES,
+    });
+  },
+  create: async (req, res) => {
+    if (req.method === "POST") {
+      const data = await Todo.create(req.body);
+      if (data) res.redirect("/view");
+    } else {
+      res.render("todoCreate", { priorities: PRIORITIES });
+    }
+  },
+  read: async (req, res) => {
+    const data = await Todo.findByPk(req.params.id);
+
+    res.render("todoRead", { todo: data, priorities: PRIORITIES });
+  },
+
+  update: async (req, res) => {
+    if (req.method === "POST") {
+      const data = await Todo.update(req.body, {
+        where: { id: req.params.id },
+      });
+
+      if (data) res.redirect("/view");
+    } else {
+      const data = await Todo.findByPk(req.params.id);
+
+      res.render("todoUpdate", { todo: data, priorities: PRIORITIES });
+    }
+  },
+
+  delete: async (req, res) => {
+    //todo -> cancel yapildiginda datayi silmesini nasil engelleriz ?
+
+    const data = await Todo.destroy({ where: { id: req.params.id } });
+
+    if (data > 0) {
+      // kayıt silindiyse...
+
+      res.redirect("/view");
+    } else {
+      // silinemediyse...
+
+      // send to ErrorHandler:
+      res.errorStatusCode = 404;
+      throw new Error("Can not Deleted. (Maybe Already deleted)");
+    }
+  },
+};
